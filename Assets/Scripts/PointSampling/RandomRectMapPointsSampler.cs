3ac30e50e18eae512c49d6e00a711927dbe6b08d
Dimitri Lagast <lagast.dimitri@gmail.com>
2023-01-19 22:15:47 +0100

    Initial total random rectangular points sampler

---
+++ Assets/Scripts/PointSampling/RandomRectMapPointsSampler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using DelaunatorSharp;
+using DelaunatorSharp.Unity;
+using DelaunatorSharp.Unity.Extensions;
+using System.Collections.Generic;
+using System.Linq;
+
+[CreateAssetMenu(menuName = "MapGeneration/RandomRectMapPointsSampler")]
+public class RandomRectMapPointsSampler : MapPointsSampler
+{
+    public Vector2 size = Vector2.one;
+    public int points = 100;
+
+    public override List<IPoint> Sample()
+    {
+        List<Vector2> pointList = new();
+        for(int i = 0; i < points; i++)
+        {
+            var p = new Vector2(Random.Range(0, 1), Random.Range(0, 1));
+            p = p * size - size / 2;
+            pointList.Add(p);
+        }
+
+        return pointList.ToPoints().ToList();
+    }
+}
