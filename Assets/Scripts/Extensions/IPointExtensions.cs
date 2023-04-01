651ae2603dceb73fef784ff6fca456e434d5ded6
Dimitri Lagast <lagast.dimitri@gmail.com>
2023-04-01 17:57:54 +0200

    Extensions for Vector2 and Point conversions

---
+++ Assets/Scripts/Extensions/IPointExtensions.cs
@@ -0,0 +1,17 @@
+﻿using DelaunatorSharp;
+
+namespace UnityEngine
+{
+    public static class IPointExtensions
+    {
+        public static Vector2 ToVector2(this IPoint point)
+        {
+            return new Vector2((float)point.X, (float)point.Y);
+        }
+
+        public static float DistanceTo(this IPoint point1, IPoint point2)
+        {
+            return Vector2.Distance(point1.ToVector2(), point2.ToVector2());
+        }
+    }
+}
---
+++ Assets/Scripts/Extensions/Vector2Extensions.cs
@@ -0,0 +1,12 @@
+﻿using DelaunatorSharp;
+
+namespace UnityEngine
+{
+    public static class Vector2Extensions
+    {
+        public static IPoint ToPoint(this Vector2 point)
+        {
+            return new Point((double)point.x, (double)point.y);
+        }
+    }
+}
